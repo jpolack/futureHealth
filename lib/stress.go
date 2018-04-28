6bc3e135315a602cc80d288ed703445aa71979aa
Jonah Polack <jonah.polack@cleverbridge.com>
2018-04-28 21:55:16 +0200

    refactor

--- lib/stress.go
+++ lib/stress.go
@@ -1,5 +1,7 @@
 package lib
 
+//DEPRECATED
+
 import (
 	"time"
 )
--- main.go
+++ main.go
@@ -14,9 +14,6 @@ func main() {
 	achievPers := lib.CreateAchievmentPersistence("./data/achievments.json")
 	achievHandler := lib.AchievmentHandler{&achievPers}
 
-	stressPers := lib.CreateStressPersistence("./data/stress.json")
-	stressHandler := lib.StressLevelHandler{&stressPers}
-
 	userPersistence := lib.CreateUserPersistence("./data/users.json")
 	runtasticApi := api.RuntasticApi{}
 	userHandler := lib.UserHandler{&userPersistence, &runtasticApi}
@@ -42,21 +39,6 @@ func main() {
 		}
 		c.Set("userId", token)
 	})
-	app.POST("/stress", func(c *gin.Context) {
-		stress := lib.StressLevel{}
-		bodyDecoder := json.NewDecoder(c.Request.Body)
-		err := bodyDecoder.Decode(&stress)
-		if err != nil {
-			c.JSON(400, "Invalid JSON")
-			return
-		}
-		userIdBlob, found := c.Get("userId")
-		if !found {
-			c.JSON(401, "Authentication required")
-		}
-		stressHandler.Create(stress, userIdBlob.(string))
-		c.JSON(200, "OK")
-	})
 	app.GET("/points", func(c *gin.Context) {
 		userIdBlob, found := c.Get("userId")
 		if !found {
@@ -71,27 +53,6 @@ func main() {
 		}
 		achievments := achievHandler.Read()
 		c.JSON(200, userHandler.UserAchieved(achievments, userIdBlob.(string)))
-	})
-	app.POST("/runtastic", func(c *gin.Context) {
-		// credentials := lib.Credentials{}
-		// bodyDecoder := json.NewDecoder(c.Request.Body)
-		// err := bodyDecoder.Decode(&credentials)
-		// if err != nil {
-		// 	c.JSON(400, "Invalid JSON")
-		// 	return
-		// }
-
-		// userIdBlob, found := c.Get("userId")
-		// if !found {
-		// 	c.JSON(401, "Authentication required")
-		// }
-
-		// err = userHandler.RuntasticLogin(credentials, userIdBlob.(string))
-		// if err != nil {
-		// 	c.JSON(400, "Invalid Login")
-		// 	return
-		// }
-		c.JSON(200, "OK")
 	})
 
 	admin := r.Group("/admin")
@@ -110,10 +71,6 @@ func main() {
 
 		achievHandler.Create(achiev)
 		c.JSON(200, "OK")
-	})
-	admin.GET("/stress", func(c *gin.Context) {
-		stress := stressHandler.Read()
-		c.JSON(200, stress)
 	})
 
 	r.Run(":3000")
