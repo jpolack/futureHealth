cd3ee0afddb0494b6d2a298fecca8e016c08a6dc
Jonah Polack <jonah.polack@cleverbridge.com>
2018-04-28 22:53:32 +0200

    revert

---
+++ lib/stress.go
@@ -0,0 +1,29 @@
+package lib
+
+import (
+	"time"
+)
+
+type StressLevel struct {
+	Percent float64   `json:"percent"`
+	Date    time.Time `json:"date"`
+}
+
+type StressPersistence interface {
+	read() []StressLevel
+	save([]StressLevel)
+}
+
+type StressLevelHandler struct {
+	Pers StressPersistence
+}
+
+func (s *StressLevelHandler) Create(stress StressLevel, userId string) {
+	levels := s.Pers.read()
+	levels = append(levels, stress)
+	s.Pers.save(levels)
+}
+func (s *StressLevelHandler) Read() []StressLevel {
+	levels := s.Pers.read()
+	return levels
+}
--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	achievPers := lib.CreateAchievmentPersistence("./data/achievments.json")
 	achievHandler := lib.AchievmentHandler{&achievPers}
 
+	stressPers := lib.CreateStressPersistence("./data/stress.json")
+	stressHandler := lib.StressLevelHandler{&stressPers}
+
 	userPersistence := lib.CreateUserPersistence("./data/users.json")
 	runtasticApi := api.RuntasticApi{}
 	userHandler := lib.UserHandler{&userPersistence, &runtasticApi}
@@ -39,6 +42,21 @@ func main() {
 		}
 		c.Set("userId", token)
 	})
+	app.POST("/stress", func(c *gin.Context) {
+		stress := lib.StressLevel{}
+		bodyDecoder := json.NewDecoder(c.Request.Body)
+		err := bodyDecoder.Decode(&stress)
+		if err != nil {
+			c.JSON(400, "Invalid JSON")
+			return
+		}
+		userIdBlob, found := c.Get("userId")
+		if !found {
+			c.JSON(401, "Authentication required")
+		}
+		stressHandler.Create(stress, userIdBlob.(string))
+		c.JSON(200, "OK")
+	})
 	app.GET("/points", func(c *gin.Context) {
 		userIdBlob, found := c.Get("userId")
 		if !found {
@@ -53,6 +71,27 @@ func main() {
 		}
 		achievments := achievHandler.Read()
 		c.JSON(200, userHandler.UserAchieved(achievments, userIdBlob.(string)))
+	})
+	app.POST("/runtastic", func(c *gin.Context) {
+		// credentials := lib.Credentials{}
+		// bodyDecoder := json.NewDecoder(c.Request.Body)
+		// err := bodyDecoder.Decode(&credentials)
+		// if err != nil {
+		// 	c.JSON(400, "Invalid JSON")
+		// 	return
+		// }
+
+		// userIdBlob, found := c.Get("userId")
+		// if !found {
+		// 	c.JSON(401, "Authentication required")
+		// }
+
+		// err = userHandler.RuntasticLogin(credentials, userIdBlob.(string))
+		// if err != nil {
+		// 	c.JSON(400, "Invalid Login")
+		// 	return
+		// }
+		c.JSON(200, "OK")
 	})
 
 	admin := r.Group("/admin")
@@ -71,6 +110,10 @@ func main() {
 
 		achievHandler.Create(achiev)
 		c.JSON(200, "OK")
+	})
+	admin.GET("/stress", func(c *gin.Context) {
+		stress := stressHandler.Read()
+		c.JSON(200, stress)
 	})
 
 	r.Run(":3000")
